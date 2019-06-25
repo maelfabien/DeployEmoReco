739ddf26a3a940d8a95914c085f50ba89bdbff72
Maël <mael.fabien@gmail.com>
2019-06-25 16:37:46 +0200

    J

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import requests
 from flask import Flask, render_template, session, request, redirect, flash, Response
 
 ### Audio imports ###
-from library.speech_emotion_recognition import *
+#from library.speech_emotion_recognition import *
 
 ### Video imports ###
 from library.video_emotion_recognition import *
@@ -189,87 +189,6 @@ def video_dash():
     
     return render_template('video_dash.html', emo=emotion_label(emotion), emo_other = emotion_label(emotion_other), prob = emo_prop(df_2), prob_other = emo_prop(df))
 
-
-################################################################################
-############################### AUDIO INTERVIEW ################################
-################################################################################
-
-# Audio Index
-@app.route('/audio_index', methods=['POST'])
-def audio_index():
-
-    # Flash message
-    flash("After pressing the button above, you will have 15sec to answer the question.")
-    
-    return render_template('audio.html', display_button=False)
-
-# Audio Recording
-@app.route('/audio_recording', methods=("POST", "GET"))
-def audio_recording():
-
-    # Instanciate new SpeechEmotionRecognition object
-    SER = speechEmotionRecognition()
-
-    # Voice Recording
-    rec_duration = 16 # in sec
-    rec_sub_dir = os.path.join('tmp','voice_recording.wav')
-    SER.voice_recording(rec_sub_dir, duration=rec_duration)
-
-    # Send Flash message
-    flash("The recording is over! You now have the opportunity to do an analysis of your emotions. If you wish, you can also choose to record yourself again.")
-
-    return render_template('audio.html', display_button=True)
-
-
-# Audio Emotion Analysis
-@app.route('/audio_dash', methods=("POST", "GET"))
-def audio_dash():
-
-    # Sub dir to speech emotion recognition model
-    model_sub_dir = os.path.join('Models', 'audio.hdf5')
-
-    # Instanciate new SpeechEmotionRecognition object
-    SER = speechEmotionRecognition(model_sub_dir)
-
-    # Voice Record sub dir
-    rec_sub_dir = os.path.join('tmp','voice_recording.wav')
-
-    # Predict emotion in voice at each time step
-    step = 1 # in sec
-    sample_rate = 16000 # in kHz
-    emotions, timestamp = SER.predict_emotion_from_file(rec_sub_dir, chunk_step=step*sample_rate)
-
-    # Export predicted emotions to .txt format
-    SER.prediction_to_csv(emotions, os.path.join("static/js/db", "audio_emotions.txt"), mode='w')
-    SER.prediction_to_csv(emotions, os.path.join("static/js/db", "audio_emotions_other.txt"), mode='a')
-
-    # Get most common emotion during the interview
-    major_emotion = max(set(emotions), key=emotions.count)
-
-    # Calculate emotion distribution
-    emotion_dist = [int(100 * emotions.count(emotion) / len(emotions)) for emotion in SER._emotion.values()]
-
-    # Export emotion distribution to .csv format for D3JS
-    df = pd.DataFrame(emotion_dist, index=SER._emotion.values(), columns=['VALUE']).rename_axis('EMOTION')
-    df.to_csv(os.path.join('static/js/db','audio_emotions_dist.txt'), sep=',')
-
-    # Get most common emotion of other candidates
-    df_other = pd.read_csv(os.path.join("static/js/db", "audio_emotions_other.txt"), sep=",")
-
-    # Get most common emotion during the interview for other candidates
-    major_emotion_other = df_other.EMOTION.mode()[0]
-
-    # Calculate emotion distribution for other candidates
-    emotion_dist_other = [int(100 * len(df_other[df_other.EMOTION==emotion]) / len(df_other)) for emotion in SER._emotion.values()]
-
-    # Export emotion distribution to .csv format for D3JS
-    df_other = pd.DataFrame(emotion_dist_other, index=SER._emotion.values(), columns=['VALUE']).rename_axis('EMOTION')
-    df_other.to_csv(os.path.join('static/js/db','audio_emotions_dist_other.txt'), sep=',')
-
-    # Sleep
-    time.sleep(0.5)
-
-    return render_template('audio_dash.html', emo=major_emotion, emo_other=major_emotion_other, prob=emotion_dist, prob_other=emotion_dist_other)
 
 
 ################################################################################
